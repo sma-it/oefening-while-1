22d171320ad8aeaa6e6f67da3381b22b2ebc50db
tinahauq <40709994+tinahauq@users.noreply.github.com>
2018-08-22 10:29:45 +0200

    Added exercises

---
+++ oefening/Program.cs
@@ -0,0 +1,53 @@
+using System;
+
+namespace First
+{
+    class Program
+    {
+        /* Verdubbel het functieargument tot het getal 1000 overschreden wordt. Geef het laatste product dat je bekomt
+          * als functieresultaat.
+          */
+        public static int VerdubbelGetal(int a)
+        {
+            return 0;
+        }
+
+        /* Kwadrateer het eerste functieargument tot het resultaat groter wordt dan het tweede functieargument.
+         * Geef als functieresultaat hoe vaak je hebt moeten kwadrateren.
+         * Tip: gebruik de functie Math.Pow() bij het kwadrateren.
+		 */
+        public static int KwadrateerGetal(double a, double b)
+        {
+            return 0;
+        }
+
+        /* Het aantal leeuwen in een natuurpark wordt als functieargument doorgegeven. Het aantal groeit jaarlijks met 15%. 
+         * Hoeveel jaren duurt het tot er meer dan 1000 leeuwen zijn? Geef dit aantal jaren als functieresultaat.
+		 */
+        public static int BepaalLeeuwen(double aantal)
+        {
+            return 0;
+        }
+
+
+        /* Bepaal de Fibonaci nummers en tel deze op tot de som het opgegeven maximum (functieargument) overschreidt.
+         * Als functieresultaat geef je de som die het opgegeven maximum overschreidt.
+         */
+
+        public static int SomFibonaci(int max)
+        {
+            return 0;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("VerdubbelGetal \t\t 1280 \t\t\t" + VerdubbelGetal(20));
+            Console.WriteLine("KwadrateerGetal \t 3 \t\t\t" + KwadrateerGetal(5, 10000));
+            Console.WriteLine("BepaalLeeuwen \t\t 33 \t\t\t" + BepaalLeeuwen(10));
+            Console.WriteLine("SomFibonaci \t\t 88 \t\t\t" + SomFibonaci(30));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ oefening/Test.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using NUnitLite;
+
+namespace Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        // deze functie is nodig om de testen achteraf via de webinterface uit te voeren
+        public static int Run(string resultPath)
+        {
+            string[] args = { "--work=" + resultPath };
+            return new AutoRun().Execute(args);
+        }
+
+        [Test]
+        public void TestVerdubbelGetal()
+        {
+            Assert.That(First.Program.VerdubbelGetal(15), Is.EqualTo(1920));
+        }
+
+        [Test]
+        public void TestKwadrateerGetal()
+        {
+            Assert.That(First.Program.KwadrateerGetal(3, 100000), Is.EqualTo(4));
+        }
+
+        [Test]
+        public void TestBepaalLeeuwen()
+        {
+            Assert.That(First.Program.BepaalLeeuwen(27), Is.EqualTo(26));
+        }
+
+        [Test]
+        public void TestSomFibonaci()
+        {
+            Assert.That(First.Program.SomFibonaci(300), Is.EqualTo(986));
+        }
+    }
+}
